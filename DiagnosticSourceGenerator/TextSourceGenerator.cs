4b79644c3c22d2f7dfd683694dc041d6dd0a0d21
Paul Turner <programming.hero@gmail.com>
2024-08-10 15:24:44 +0100

    Add project files.

---
+++ DiagnosticSourceGenerator/TextSourceGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace DiagnosticSourceGenerator
+{
+    [Generator(LanguageNames.CSharp)]
+    public class TextSourceGenerator : IIncrementalGenerator
+    {
+        public static readonly DiagnosticDescriptor TextFileEmpty = new DiagnosticDescriptor(
+            id: "DG100",
+            title: "Text file empty",
+            messageFormat: "The input has no content",
+            "DiagnosticSourceGenerator",
+            DiagnosticSeverity.Error,
+            isEnabledByDefault: true);
+
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            var diagnostics = context.AdditionalTextsProvider.Select((text, cancellationToken) =>
+            {
+                return Diagnostic.Create(
+                    TextFileEmpty,
+                    Location.Create(text.Path, TextSpan.FromBounds(0, 0), new LinePositionSpan(LinePosition.Zero, LinePosition.Zero)));
+            });
+
+            context.RegisterSourceOutput(diagnostics, (sourceProduction, diagnostic) => sourceProduction.ReportDiagnostic(diagnostic));
+        }
+    }
+}
